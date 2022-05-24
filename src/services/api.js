95565edaab693fa8c9b0ce81fd0096fb95c87511
leandrojamir <leandrojamir@hotmail.com>
2022-05-24 15:44:05 -0300

    Iniciando projeto e refetuando requisito 1, teste 1/1 ok

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
+// 1. Implemente o módulo de acesso à api do Mercado Livre
 export async function getCategories() {
-  // Implemente aqui
+  const resposta = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const respostaJson = await resposta.json();
+  return respostaJson;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const resposta = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const respostaJson = await resposta.json();
+  return respostaJson;
 }
