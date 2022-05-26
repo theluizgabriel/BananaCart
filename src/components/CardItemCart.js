f153f312fe6a105cf6e9614d681ad24404535dcb
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-05-26 16:31:21 -0300

    requisito 10: altera quantidade de itens dentro do carrinho

--- src/components/CardItemCart.js
+++ src/components/CardItemCart.js
@@ -3,21 +3,41 @@ import PropTypes from 'prop-types';
 
 class CardItemCart extends React.Component {
   render() {
-    const { name, image, qty } = this.props;
+    const { id, name, image, qty, price, addOrRemoveItem } = this.props;
     return (
-      <li>
+      <li style={ { border: '1px solid black' } }>
         <p data-testid="shopping-cart-product-name">{name}</p>
         <img src={ image } alt={ name } />
-        <p data-testid="shopping-cart-product-quantity">{qty}</p>
+        <p data-testid="shopping-cart-product-quantity">{ `Quantidade: ${qty}` }</p>
+        <p>{ `R$ ${price}` }</p>
+        <button
+          onClick={ () => addOrRemoveItem('remove', { id, name, price, image }) }
+          id={ id }
+          data-testid="product-decrease-quantity"
+          type="button"
+        >
+          -1
+        </button>
+        <button
+          onClick={ () => addOrRemoveItem('add', { id, name, price, image }) }
+          id={ id }
+          data-testid="product-increase-quantity"
+          type="button"
+        >
+          +1
+        </button>
       </li>
     );
   }
 }
 
 CardItemCart.propTypes = {
+  id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
   qty: PropTypes.number.isRequired,
+  addOrRemoveItem: PropTypes.func.isRequired,
 };
 
 export default CardItemCart;
