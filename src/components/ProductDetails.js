046b395af51bdbe213e0a64ce31f6b002c881c38
Luiz Gabriel <lgabrielpessoal@gmail.com>
2022-05-26 16:00:01 -0300

    Requisito 11 parcial

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -3,6 +3,36 @@ import PropTypes from 'prop-types';
 import CartPageRedirect from './CartPageRedirect';
 
 class ProductDetails extends Component {
+  state = {
+    userEmail: '',
+    userReview: '',
+    rating: '',
+    reviews: [],
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
+  resetReview = (card) => {
+    this.setState((prev) => ({
+      reviews: [...prev.reviews, card],
+      userEmail: '',
+      userReview: '',
+      rating: '',
+    }));
+  }
+
+  saveReview = () => {
+    const { userEmail, userReview, rating } = this.state;
+    const reviewObject = {
+      email: userEmail,
+      review: userReview,
+      rating,
+    };
+    this.resetReview(reviewObject);
+  }
+
   render() {
     const {
       addToCart,
@@ -12,6 +42,7 @@ class ProductDetails extends Component {
         },
       },
     } = this.props;
+    const { userEmail, userReview, reviews } = this.state;
     return (
       <div>
         <p data-testid="product-detail-name">{name}</p>
@@ -25,6 +56,86 @@ class ProductDetails extends Component {
         >
           Adicionar ao Carrinho
         </button>
+        <h2>Avaliações</h2>
+        <form>
+          <input
+            type="email"
+            data-testid="product-detail-email"
+            name="userEmail"
+            placeholder="E-mail"
+            value={ userEmail }
+            onChange={ this.handleChange }
+          />
+          <textarea
+            data-testid="product-detail-evaluation"
+            name="userReview"
+            placeholder="Mensagem (opcional)"
+            value={ userReview }
+            onChange={ this.handleChange }
+          />
+          <label htmlFor="numberOne">
+            1
+            <input
+              type="radio"
+              name="rating"
+              value="1"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="numberTwo">
+            2
+            <input
+              type="radio"
+              name="rating"
+              value="2"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="numberThree">
+            3
+            <input
+              type="radio"
+              name="rating"
+              value="3"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="numberFour">
+            4
+            <input
+              type="radio"
+              name="rating"
+              value="4"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="numberFive">
+            5
+            <input
+              type="radio"
+              name="rating"
+              value="5"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="submit-review-btn"
+            name="review-button"
+            onClick={ this.saveReview }
+          >
+            Avaliar
+          </button>
+        </form>
+        {reviews.map((element, index) => (
+          <div key={ index }>
+            <p>{element.email}</p>
+            <p>{element.review}</p>
+            <img src="../star.png" alt="star" />
+            <p>{element.rating}</p>
+            <hr />
+          </div>
+        ))}
       </div>
     );
   }
